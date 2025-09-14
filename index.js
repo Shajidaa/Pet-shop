9da995fa3d5420b8445a0152c344950f2262d63e
Shajidaa <shajidaakterlopa101@gmail.com>
2025-09-14 10:27:04 +0600

    update

--- index.js
+++ index.js
@@ -87,16 +87,16 @@ const displayPetCategories = (pets) => {
               <h1 class="text-2xl font-bold mb-2">${pet.pet_name}</h1>
               <div>
                 <p class="text-[#6e6c6c] font-normal text-base mb-2">
-                  <i class="fa-regular fa-square"></i> Breed :
+                  <i class="fa-regular fa-square"></i>  Breed :${pet.breed}
                 </p>
                 <p class="text-[#6e6c6c] font-normal text-base mb-2">
-                  <i class="fa-solid fa-calendar"></i> Birth :
+                  <i class="fa-solid fa-calendar"></i> Birth : ${pet.date_of_birth}
                 </p>
                 <p class="text-[#6e6c6c] font-normal text-base mb-2">
-                  <i class="fa-solid fa-mars-and-venus"></i> Gender :
+                  <i class="fa-solid fa-mars-and-venus"></i> Gender : ${pet.gender}
                 </p>
                 <p class="text-[#6e6c6c] font-normal text-base mb-2">
-                  $ Price :
+                  $ Price :  ${pet.price}
                 </p>
               </div>
             </div>
@@ -110,7 +110,7 @@ const displayPetCategories = (pets) => {
               >
                 Adopt
               </p>
-              <p
+              <p onclick='details(${pet.petId})'
                 class="btn border-1 rounded-lg border-[#0E7A81] text-[#0E7A81] font-bold lg:text-lg text-base"
               >
                 Details
@@ -121,11 +121,76 @@ const displayPetCategories = (pets) => {
     `
    displayImage.innerHTML+=`
     <div>
-              <img class="mb-5 w-full rounded-xl" src="${pet.image}" alt="pet image" />
+     <img class="mb-5 w-full rounded-xl" src="${pet.image}" alt="pet image" />
             </div>
    `
+   
   })
 }
 
+const details=async(id)=>{
+ const url=await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${id}`)
+ const data=await url.json();
+ displayDetails(data.petData);
+ 
+}
+// "breed": "Golden Retriever",
+// "category": "Dog",
+// "date_of_birth": "2023-01-15",
+// "price": 1200,
+// "image": "https://i.ibb.co.com/p0w744T/pet-1.jpg",
+// "gender": "Male",
+// "pet_details": "This friendly male Golden Retriever is energetic and loyal, making him a perfect companion for families. Born on January 15, 2023, he enjoys playing outdoors and is especially great with children. Fully vaccinated, he's ready to join your family and bring endless joy. Priced at $1200, he offers love, loyalty, and a lively spirit for those seeking a playful yet gentle dog.",
+// "vaccinated_status": "Fully",
+// "pet_name": "Sunny"
+
+
+const displayDetails=(pet)=>{
+  console.log(pet);
+  document.getElementById('detailsContainer').innerHTML='';
+document.getElementById('detailsContainer').innerHTML+=`
+   <div class="container border-1 border-gray-200 p-5 rounded-xl shadow">
+            <div>
+              <img class="mb-5 w-full rounded-xl" src="${pet.image}" alt="pet image" />
+            </div>
+            <div>
+              <h1 class="text-2xl font-bold mb-2">${pet.pet_name}</h1>
+              <div>
+                <p class="text-[#6e6c6c] font-normal text-base mb-2">
+                  <i class="fa-regular fa-square"></i> Breed :${pet.breed}
+                </p>
+                <p class="text-[#6e6c6c] font-normal text-base mb-2">
+                  <i class="fa-solid fa-calendar"></i> Birth : ${pet.date_of_birth}
+                </p>
+                <p class="text-[#6e6c6c] font-normal text-base mb-2">
+                  <i class="fa-solid fa-mars-and-venus"></i> Gender : ${pet.gender}
+                </p>
+                <p class="text-[#6e6c6c] font-normal text-base mb-2">
+                  $ Price :  ${pet.price}
+                </p>
+              </div>
+              </div> <P>Details information :${pet.pet_details}</p>
+            </div>
+            <hr class="text-gray-300" /> 
+                
+          </div>
+          </div>
+            <div class="modal-action">
+              <form method="dialog">
+                <!-- if there is a button in form, it will close the modal -->
+                <button class="btn w-full btn-wide">Close</button>
+              </form>
+            </div>
+
+
+`
+document.getElementById("my_modal_5").showModal();
+  
+}
+
+
+
+
 loadPetCategories()
 loadCategories()
+
